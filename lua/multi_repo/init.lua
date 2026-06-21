26f5d5b5d6732875fa6c9e0e1fc08c6e081958c3
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-06-21 15:31:09 +0200

    init

---
+++ lua/multi_repo/init.lua
@@ -0,0 +1,29 @@
+local Config = require("multi_repo.config")
+local Git = require("multi_repo.git")
+local Scanner = require("multi_repo.scanner")
+local Telescope = require("multi_repo.telescope")
+
+local MultiRepo = {}
+
+local function open_picker()
+    local cwd = vim.fn.getcwd()
+    local scan_root = Git.get_root(cwd) or cwd
+    local repositories = Scanner.scan(scan_root)
+
+    if #repositories == 0 then
+        vim.notify("No repositories found", vim.log.levels.WARN)
+        return
+    end
+
+    Telescope.open(repositories)
+end
+
+function MultiRepo.setup(user_config)
+    Config.setup(user_config)
+
+    vim.api.nvim_create_user_command("MultiRepo", function()
+        open_picker()
+    end, {})
+end
+
+return MultiRepo
